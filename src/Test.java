29f52db7ddb9a85ed332b6cf008c29d49391ca32
Minimals Rock <kenkarlson@mail.ru>
2019-10-16 19:36:00 +0300

    Add new file test and edit task

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Test {
+    public static void main(String[] args) throws ParseException {
+        DateFormat myFormat = new SimpleDateFormat("yyyyMMddHHmm"); // Формат даты
+        // Из строки в тайм
+        String myData = "201901011320"; // Входящие данные
+        var incomingTime = myFormat.parse(myData);// Получить дату
+        System.out.println(incomingTime);// Распечатать дату
+
+        
+        // Из тайм в строку
+        var currentTime = myFormat.format(System.currentTimeMillis());// Текущая дата
+        System.out.println(currentTime);// Текущая дата в моем формате
+
+
+    }
 }
