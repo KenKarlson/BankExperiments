370dd32875d58c4d9cc6b6c2f91410301ed47e4b
Minimals Rock <kenkarlson@mail.ru>
2019-10-18 22:00:05 +0300

    Final home work vers. 1.0

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,50 @@
 //*
 
 import bankaccounts.BankAccount;
+import bankaccounts.CardAccount;
+import bankaccounts.DebitAccount;
+import org.w3c.dom.ls.LSOutput;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         BankAccount bk_1 = new BankAccount(001, 1000.00);
-        System.out.println(bk_1.getTotalMoney());
+        System.out.println("Сколько средств на основном счете?");
+        System.out.println("На счете "+bk_1.getTotalMoney()+" руб.");
+        System.out.println("Выдача наличных");
+        bk_1.withDrawMoney(420);
 
-        bk_1.withDrawMoney(500);
+        System.out.println("* * * * * * * *");
+        DebitAccount dk_1 = new DebitAccount(002, 5000, "201908181200");
+        System.out.println("Сколько средств на дебетовом счете?");
+        System.out.println("На счете "+dk_1.getTotalMoney()+" руб.");
+        long diff = TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis())-
+                TimeUnit.MILLISECONDS.toDays(new SimpleDateFormat("yyyyMMddHHmm")
+                        .parse(dk_1.openingDate).getTime());
+        System.out.println("Выдача наличных");
+        dk_1.withDrawMoney(500, diff);
+        System.out.println("* * * * * * * *");
+        DebitAccount dk_2 = new DebitAccount(003, 5000, "201910171200");
+        System.out.println("Снятие наличный со счете открытого менее месяца назад...");
+        long diff_2 = TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis())-
+                TimeUnit.MILLISECONDS.toDays(new SimpleDateFormat("yyyyMMddHHmm")
+                        .parse(dk_2.openingDate).getTime());
+        dk_2.withDrawMoney(500, diff_2);
+
+        System.out.println("* * * * * * * *");
+
+        CardAccount ck_1 = new CardAccount(004,1000);
+        System.out.print("Номер карточного счета > ");
+        System.out.println(ck_1.getAccountNumber());
+        System.out.println("Сколько средств на карточном счете?");
+        System.out.println("На счете "+ck_1.getTotalMoney()+" руб.");
+        ck_1.withDrawMoney(100);
+
+
 
     }
 }
--- src/bankaccounts/DebitAccount.java
+++ src/bankaccounts/DebitAccount.java
@@ -1,7 +1,38 @@
 package bankaccounts;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
 public class DebitAccount extends BankAccount{
-    public DebitAccount(int accountNumber, double totalMoney) {
+    DateFormat myFormat = new SimpleDateFormat("yyyyMMddHHmm");
+
+    public String openingDate ;
+    public DebitAccount(int accountNumber, double totalMoney) throws ParseException {
         super(accountNumber, totalMoney);
+    }
+    public DebitAccount(int accountNumber, double totalMoney, String openingDate) throws ParseException {
+        super(accountNumber, totalMoney);
+        this.openingDate = openingDate;
+    }
+
+    public String getOpeningDate() {
+        return openingDate;
+    }
+
+
+    // Выдать запрошенную сумму
+    public double withDrawMoney(double x, long diff ) {
+
+        if (diff >= 31 && x < totalMoney && totalMoney > 0) {
+            totalMoney = totalMoney - x;
+            System.out.println("Выдано > " + x);
+            System.out.println("Остаток на счете: " + totalMoney);
+            return x;
+
+        }
+        System.out.println("Операция невозможна...");
+        return 0.0;
     }
 }
